8380c65b3849541c13678e5720acefd3225fdcf7
redcodemohammed <redcodemohammed@gmail.com>
2024-04-26 15:57:43 +0300

    Add @next/env package and update NextAuth configuration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,16 @@
 import NextAuth from 'next-auth'
 import Github from 'next-auth/providers/github'
+import '@/envConfig'
+
 const handler = NextAuth({
   providers: [
     Github({
+      // these should not be hardcoded in a real app, but for the sake of this example we will hardcode them
       clientId: '0ba9f4f7fd7c1abde987',
       clientSecret: '9ae65a4bbc8881f68b3169055365fe090844210a'
     })
-  ]
+  ],
+  secret: process.env.NEXTAUTH_SECRET
 })
 
 export { handler as GET, handler as POST }
